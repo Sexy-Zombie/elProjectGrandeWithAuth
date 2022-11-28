4d0643661c41b68d71b931250a3270ef2b27f42d
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-28 14:04:05 +0100

    SearchComponent is added.

--- ClientApp/src/components/SearchComponent.js
+++ ClientApp/src/components/SearchComponent.js
@@ -1,1 +1,28 @@
-﻿
+﻿import React from 'react';
+import { baseUrl } from './BaseUrl';
+
+
+export function SearchComponent(getAllPosts) {
+
+    async function searchByWord() {
+        let field = document.querySelector("#searchField");
+        let searchWord = field.value;
+        console.log(searchWord);
+
+        if (searchWord == "") {
+            getAllPosts();
+        }
+        else {
+            getAllPosts(searchWord);
+        }
+    }
+
+
+    return (
+        <div>
+            <input id ="searchField" type="search" placeholder="Search" />
+            <button type="button" onClick={() => searchByWord()}>Search</button>
+        </div>
+        );
+
+}

1d3223aa5d8455c7d707c10cbd22354c10846606
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-28 14:00:19 +0100

    GetAllPosts method of HomePage.js is upgraded with a searchable word.

--- ClientApp/src/components/HomePage.js
+++ ClientApp/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component, useEffect, useState, useRef } from 'react';
 import '../custom.css'
 import '../index.css'
 import { CommentComponent } from './CommentComponent';
+import { SearchComponent } from './SearchComponent'
 import { baseUrl } from './BaseUrl';
 import { apiPost } from './ApiPost';
 
@@ -15,11 +16,20 @@ export function HomeComponent() {
         getAllPosts();
     }, [])
 
-    async function getAllPosts() {
+    async function getAllPosts(word) {
 
-        const response = await fetch(`${baseUrl()}api/getAllPost`)
-        let actualData = await response.json();
-        setPosts(actualData);
+        if (word == null) {
+            const response = await fetch(`${baseUrl()}api/getAllPost`)
+            let actualData = await response.json();
+            setPosts(actualData);
+        }
+
+        else {
+            const response = await fetch(`${baseUrl()}api/getPosts/${word}`)
+            let actualData = await response.json();
+            setPosts(actualData);
+        }
+        
     }
 
     async function AddCommentToPost(id) {
@@ -80,6 +90,7 @@ export function HomeComponent() {
             <div className="title">
                 <h1>War Thunder Forum</h1>
             </div>
+            {SearchComponent(getAllPosts)}
             <div className="add-post-block">
                 {getUsername()}
                 <a href="/add-post"><button className="add-post-btn" type="button">Add post</button></a>

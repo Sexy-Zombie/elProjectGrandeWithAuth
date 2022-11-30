641a70add642943bfc72e807d732998c3d4b7216
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-30 09:30:21 +0100

    SaveJwtToken and LoginUser functions are added to LoginPage

--- ClientApp/src/components/LoginPage.js
+++ ClientApp/src/components/LoginPage.js
@@ -1,32 +1,37 @@
 ﻿import { useNavigate } from 'react-router-dom';
+import { apiPost } from './ApiPost';
+import { baseUrl } from './BaseUrl';
 
 export function LoginPage(props) {
-    let baseUrl = "https://localhost:44449/"
     const navigate = useNavigate();
 
-    async function getUsers() {
-        let response = await fetch(`${baseUrl}api/getUsers`)
-        return response.json()
+    
+    function saveJwtToken(token, expiresAt) {
+        localStorage.setItem("jwt", token);
+        localStorage.setItem("jwtExpiresAt", new Date(expiresAt).toUTCString());
     }
 
-    async function validateUser() {
-        let users = await getUsers()
-        let email = document.querySelector("#email").value
+
+
+    async function loginUser() {
+        let username = document.querySelector("#username").value
         let password = document.querySelector("#password").value
 
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].email == email && users[i].password == password) {
+        let data = {
+            Username : username,
+            Password: password
+        };
+        let token = await apiPost(`${baseUrl()}Account/loginUser`, data);
 
-                sessionStorage.setItem("name", users[i].username)
-                props.setUserLoggedIn(true)
-                navigate('/');
-            } else {
-                alert ("Username or password is invalid!")
-            } 
-            
-        }
+        console.log(token.access_token);
+        console.log(token.expiresAt);
 
-        
+        saveJwtToken(token.access_token, token.expiresAt)
+
+        sessionStorage.setItem("name", username);
+        props.setUserLoggedIn(true);
+
+        navigate('/');
     }
 
     return (
@@ -40,8 +45,8 @@ export function LoginPage(props) {
                 <form>
                     <div className="form-group">
                         <label className="control-label">
-                            Email
-                            <input type="email" className="form-control" id="email" required />
+                            Username
+                                <input type="username" className="form-control" id="username" required />
                         </label>
                     </div>
                     <div className="form-group">
@@ -53,7 +58,7 @@ export function LoginPage(props) {
                     </form>
                 </div>
                 <div className="form-group login-btn-block">
-                    <button type="button" className="btn btn-primary" id="login-btn" onClick={validateUser}>Login</button>
+                    <button type="button" className="btn btn-primary" id="login-btn" onClick={loginUser}>Login</button>
                 </div>
         </div>
 

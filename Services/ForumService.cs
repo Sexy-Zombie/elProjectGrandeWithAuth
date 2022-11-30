b02278afed428fbeb4396056e74d759945ac1671
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-30 09:20:09 +0100

    User namespace is added to ForumService.

--- Services/ForumService.cs
+++ Services/ForumService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Linq;
+using WarThunderForum.Models.User;
 
 namespace WarThunderForum.Services
 {

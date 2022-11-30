06622604827066d5baeec147baa4c3a38ad3c681
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-30 21:58:55 +0100

    HomePage is refactored, separated into smaller components.

--- ClientApp/src/components/HomePage.js
+++ ClientApp/src/components/HomePage.js
@@ -3,12 +3,15 @@ import '../custom.css'
 import '../index.css'
 import { CommentComponent } from './CommentComponent';
 import { SearchComponent } from './SearchComponent'
+import { PostComponent } from './PostComponent'
 import { baseUrl } from './BaseUrl';
 import { apiPost } from './ApiPost';
+import Highlighter from "react-highlight-words";
 
 
 export function HomeComponent() {
 
+    let [searchedWord, setWord] = useState("");
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
@@ -19,63 +22,22 @@ export function HomeComponent() {
     async function getAllPosts(word) {
 
         if (word == null) {
+            setWord("");
             const response = await fetch(`${baseUrl()}api/getAllPost`)
             let actualData = await response.json();
             setPosts(actualData);
         }
 
         else {
+            setWord(word);
             const response = await fetch(`${baseUrl()}api/getPosts/${word}`)
             let actualData = await response.json();
+            
             setPosts(actualData);
         }
         
     }
-
-    async function AddCommentToPost(id) {
-
-        console.log(id);
-        let content = document.querySelector(`[data-id="comment-to-${id}"]`);
-        let comment = content.value;
-        if (comment != "") {
-            console.log(comment);
-            content.value = "";
-            let data = {
-                Content: comment,
-                Id: id
-            };
-            await apiPost(`${baseUrl()}api/addComment`, data);
-
-            getAllPosts();
-        }
-    }
-
-    async function DeletePostById(id) {
-        await fetch(`${baseUrl()}api/deletePost/${id}`)
-
-        getAllPosts();
-    }
-
-    async function AddLikeToPost(id) {
-        await fetch(`${baseUrl()}api/addLikeToPost/${id}`)
-        getAllPosts();
-    }
-
-    async function AddDislikeToPost(id) {
-        await fetch(`${baseUrl()}api/addDislikeToPost/${id}`)
-        getAllPosts();
-    }
-
-    /*async function apiPost(url, payload) {
-        let data = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(payload),
-        })
-        return await data.json()
-    }*/
+  
 
     function getUsername() {
         if (sessionStorage.getItem("name")) {
@@ -95,29 +57,7 @@ export function HomeComponent() {
                 {getUsername()}
                 <a href="/add-post"><button className="add-post-btn" type="button">Add post</button></a>
             </div>
-            <div id="forAllPosts">
-                {posts.map(post =>
-                    <div key={post.id} className="card post" data-id={post.id}>
-                        <h2> {post.title} </h2>
-                        <h3> {post.content} </h3>
-                        <h6> Likes: {post.likeCount}, Dislikes: {post.dislikeCount}, Comments: {post.commentList.length} </h6>
-                        <div className="like-buttons">
-                            <button className="add-like-btn" type="button" onClick={() => AddLikeToPost(post.id)} >Like</button>
-                            <button className="add-dislike-btn" type="button" onClick={() => AddDislikeToPost(post.id)} >Dislike</button>
-                        </div>
-                        <div className="comment-input">
-                            <input type="text" className="comment-text" placeholder="Write a new comment:" data-id={"comment-to-" + post.id} />
-                        </div>
-                        
-                        <div className="post-buttons">
-                            <button className="add-comment-btn" type="button" onClick={() => AddCommentToPost(post.id)} >Add comment</button>
-                            <button className="delete-post-btn" type="button" onClick={() => DeletePostById(post.id)} id={post.id}>Delete this post</button>
-                        </div>
-                        {CommentComponent(post, getAllPosts)}
-                        
-                    </div>
-                )}
-            </div>
+            {PostComponent(posts, getAllPosts, searchedWord)}
         </div>
     );
 }

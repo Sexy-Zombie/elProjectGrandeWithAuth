61d0981c0eba0e3bdec1fffc3ba778d559870e7d
KuraiZoltan <kz1022@gmail.com>
2022-11-30 10:50:05 +0100

    User model with hashed password

--- Models/UserAccount/User.cs
+++ Models/UserAccount/User.cs
@@ -8,7 +8,7 @@ namespace WarThunderForum.Models.User
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
 
     }
 }

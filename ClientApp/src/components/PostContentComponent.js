8ac8fe9d8bb227f6cea30512101c97d69c2f3de2
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-30 21:57:59 +0100

    PostContentComponent is added.

---
+++ ClientApp/src/components/PostContentComponent.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import '../custom.css'
+import '../index.css'
+import { CommentComponent } from './CommentComponent';
+import { baseUrl } from './BaseUrl';
+import { apiPost } from './ApiPost';
+import Highlighter from "react-highlight-words";
+
+export function PostContentComponent(content, searchedWord) {
+
+    let word = [searchedWord];
+    
+    return (
+        <Highlighter
+            highlightClassName="YourHighlightClass"
+            searchWords={word}
+            autoEscape={true}
+            textToHighlight={content}
+        />
+    );
+}

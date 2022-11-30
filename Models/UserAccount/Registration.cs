e614ddb47d5c031684f0c27baddacdf3d5f28dfb
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-30 09:25:44 +0100

    Registration and Login classes are added.

---
+++ Models/UserAccount/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WarThunderForum.Models.User
+{
+    public class Registration
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        /*[Required]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }*/
+    }
+}

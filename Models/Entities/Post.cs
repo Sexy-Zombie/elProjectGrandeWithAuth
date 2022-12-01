2ea3692e59ea74729d621ea59ead1153b965c93d
KuraiZoltan <kz1022@gmail.com>
2022-12-01 10:07:16 +0100

    Post refactoring

--- Models/Entities/Post.cs
+++ Models/Entities/Post.cs
@@ -5,5 +5,7 @@
         public string Title { get; set; }
         public List<Comment> CommentList { get; set; } = new List<Comment>();
         public string Category { get; set; }
+        
+
     }
 }

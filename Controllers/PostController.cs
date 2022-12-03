88e46053c1378d9a8e60a4cc3fa13473fbb7559f
Sexy-Zombie <szilgyigbor@gmail.com>
2022-12-03 19:47:23 +0100

    PostController is Added.

---
+++ Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarThunderForum.Models.Entities;
+using WarThunderForum.Services;
+
+namespace WarThunderForum.Controllers
+{
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly PostService _postService;
+
+        public PostController(PostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet("{id}")]
+        [Route("[controller]/getPost/{id}")]
+        public async Task<Post> GetPostById(int id)
+        {
+            return await _postService.GetPostById(id);
+        }
+
+    }
+    
+}
